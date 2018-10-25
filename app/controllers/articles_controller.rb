dda3d21a27e474151a44b6feaf2c9bd32fbd832b
William T <william.turatsinze@student.unamur.be>
2018-10-25 10:49:42 +0200

    article show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,16 +9,36 @@ def create
   flash[:notice] = "Article was successfully created"
   redirect_to article_path(@article)
   else
-    puts "omg"
    render 'new'
    end
    end
+
+def edit
+    @article = Article.find(params[:id])
+end
+
    def show
      @article = Article.find(params[:id])
    end
+
+   def update
+     @article = Article.find(params[:id])
+
+     if @article.update(article_params)
+       flash[:notice]= "Article was sucessfully updated"
+       redirect_to article_path(@article)
+     else
+
+       render 'edit'
+     end
+   end
+
+   
 private
 def article_params
   params.require(:article).permit(:title, :description)
 end
+
+
 
 end
